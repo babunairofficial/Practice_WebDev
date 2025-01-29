531e1a2bd5c47a3163a6134f084a0e5de6dc25b6
Babu Nair <babunairofficial@gmail.com>
2025-01-29 21:59:14 +0530

    guess Number Game - click event

--- Guess_Number_Game/app.js
+++ Guess_Number_Game/app.js
@@ -8,4 +8,20 @@ const input = document.querySelector("input"),
 input.focus();
 
 let randomNum = Math.floor(Math.random()*100);
+console.log(randomNum);
 chance = 10;
+
+//Click event on the check button
+checkButton.addEventListener("click", () =>{
+    //Decrease the chance variable on every click
+    chance--;
+    //value from the input field
+    let inputValue = input.value;
+    //conditional statement to check the input value is equal to the random number
+    if(inputValue == randomNum){
+        //Update guessed number, disable input, check button text and color
+        [guess.textContent, checkButton.disabled] = ["Congratulations", true];
+        [checkButton.textContent, guess.style.color] = ["Replay", "#333"]
+    }
+});
+
