bdb60df9ac2ac1d6ffb865e6f507bb323bb70eb7
Babu Nair <babunairofficial@gmail.com>
2025-01-31 17:41:26 +0530

    rockPaperScissors in javascript - index check

--- rockPaperScrissors_JavaScript/script.js
+++ rockPaperScrissors_JavaScript/script.js
@@ -5,11 +5,18 @@ const container = document.querySelector(".container"),
     result = document.querySelector(".result"),
     optionImages = document.querySelectorAll(".option_image");
 
-    console.log(container, userResult, computerResult, result, optionImages);
+    // console.log(container, userResult, computerResult, result, optionImages);
 
     //loop through each option image element
     optionImages.forEach((image, index) => {
-        image.addEventListener("click", () => {
+        image.addEventListener("click", (e) => {
             image.classList.add("active");
-        })
-    })
+
+            //Loop through each option image again
+            optionImages.forEach((image2, index2) => {
+                //If the current index doesn't match the clicked index
+                //Remove the "active" class from the other option images 
+                index !== index2 && image2.classList.remove("active");                
+            });            
+        });
+    });
