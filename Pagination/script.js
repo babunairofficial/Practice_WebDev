12204ce2051a929349a56867409a4545fc93ecee
Babu Nair <babunairofficial@gmail.com>
2025-02-02 23:27:35 +0530

    Pagination - next button js

--- Pagination/script.js
+++ Pagination/script.js
@@ -1,7 +1,7 @@
 //DOM elements selection
 const startBtn = document.querySelector("#startBtn"),
     endBtn = document.querySelector("#endBtn"),
-    prevNext = document.querySelectorAll("#prevNext"),
+    prevNext = document.querySelectorAll(".prevNext"),
     numbers = document.querySelectorAll(".link");
 
 //Setting an initial step
@@ -9,13 +9,28 @@ let currentStep = 0;
 
 //Add event listeners to the number links
 numbers.forEach((number, numIndex) => {
-    number.addEventListener("click", () => {
+    number.addEventListener("click", (e) => {
+        e.preventDefault();
         //Set the current step to the clicked number link
         currentStep = numIndex;
         console.log(currentStep);
         //Remove the "active" class from the previously active number
         document.querySelector(".active").classList.remove("active");
         //Add the "active" class to the clicked number link
-        number.classList.add("active")
+        number.classList.add("active");
+    });
+});
+
+//Add event listerners to the "Previous" and "Next" buttons
+prevNext.forEach(button => {
+    button.addEventListener("click", (e) => {
+        //Increment or decrement the current step based on the button clicked
+        currentStep += e.target.id === "next" ? 1 : -1;
+        // console.log(currentStep);
+        numbers.forEach((number, numIndex) => {
+            // console.log(numIndex, currentStep);
+            //Toggle the "active" class on the numebr links based on the button clicked
+            number.classList.toggle("active", numIndex === currentStep);
+        })
     });
 });
