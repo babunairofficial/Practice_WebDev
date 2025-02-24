2e00eac033f91ca711616c55162d4b22614a4a02
Babu Nair <babunairofficial@gmail.com>
2025-02-24 23:24:01 +0530

    website Login&Registration - view password js

--- Website_Login_Registration/script.js
+++ Website_Login_Registration/script.js
@@ -9,6 +9,21 @@ pwShowHide = document.querySelectorAll(".pw_hide");
 formOpenBtn.addEventListener("click", () => home.classList.add("show"));
 formCloseBtn.addEventListener("click", () => home.classList.remove("show"));
 
+pwShowHide.forEach(icon => {
+    icon.addEventListener("click", () => {
+        let getPwInput = icon.parentElement.querySelector("input");
+        console.log(getPwInput);
+        if(getPwInput.type === "password"){
+            getPwInput.type = "text";
+            icon.classList.replace("fa-eye-slash", "fa-eye");
+        } else{
+            getPwInput.type = "password";
+            icon.classList.replace("fa-eye", "fa-eye-slash");
+        }
+    });
+    
+});
+
 signupBtn.addEventListener("click", (e) => {
     e.preventDefault();
     formContainer.classList.add("active");
