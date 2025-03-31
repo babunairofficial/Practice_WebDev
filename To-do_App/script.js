042d1d60a0979ba213f6dd4c7252dd0087bb4576
Babu Nair <babunairofficial@gmail.com>
2025-03-31 23:47:31 +0530

    todo app - save data to local storage

--- To-do_App/script.js
+++ To-do_App/script.js
@@ -14,14 +14,22 @@ function addTask() {
         li.appendChild(span);
     }
     inputBox.value = "";
+    saveData(); //call the function to save data everytime a new data is added.
 }
 
 listContainer.addEventListener("click", function(e) {
     if(e.target.tagName === "LI"){
         e.target.classList.toggle("checked");
+        saveData(); //call the function to save data everytime task is checked or unchecked.
     }
     else if(e.target.tagName === "SPAN"){
         e.target.parentElement.remove();
+        saveData(); // call the function to save data everytime task is removed.
     }
 }, false);
 console.log(inputBox, listContainer);
+
+//save data function
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
