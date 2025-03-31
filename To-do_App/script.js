050d3ce24dd9b32aad12f25e43f2279e63a0f416
Babu Nair <babunairofficial@gmail.com>
2025-03-31 23:38:30 +0530

    todo app - checked and remove task

--- To-do_App/script.js
+++ To-do_App/script.js
@@ -1,7 +1,7 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
-function addTask(){
+function addTask() {
     if(inputBox.value === ""){
         alert("Input box cannot be left empty");
     }
@@ -15,4 +15,13 @@ function addTask(){
     }
     inputBox.value = "";
 }
+
+listContainer.addEventListener("click", function(e) {
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+}, false);
 console.log(inputBox, listContainer);
