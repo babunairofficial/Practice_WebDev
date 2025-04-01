7e795d2cac756292da146c75138c49dc09057361
Babu Nair <babunairofficial@gmail.com>
2025-04-01 10:50:45 +0530

    Quiz App - click answers

--- Quiz_App/script.js
+++ Quiz_App/script.js
@@ -70,6 +70,10 @@ function showQuestion() {
         button.innerHTML = answer.text;
         button.classList.add("btn");
         answerButtons.appendChild(button);
+        if(answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer)
     });
 }
 
@@ -79,4 +83,23 @@ function resetState() {
         answerButtons.removeChild(answerButtons.firstChild);
     }
 }
+
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect) {
+        selectedBtn.classList.add("correct");
+        score++;
+    } else {
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerButtons.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextBtn.style.display = "block";
+}
+
 startQuiz();
