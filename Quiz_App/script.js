dc9d73f9d037629e02bd3767058a358cefcabc64
Babu Nair <babunairofficial@gmail.com>
2025-04-01 10:31:00 +0530

    Quiz App - insert question and answers

---
+++ Quiz_App/script.js
@@ -0,0 +1,82 @@
+const questions = [
+    {
+        question: "The word “Mangal” is used for which among the following kinds of vegetations?",
+        answers: [
+            { text: "Aquatic Plants", correct: false},
+            { text: "Algal Blooms", correct: false},
+            { text: "Mangroves", correct: true},
+            { text: "Mediterranean forests, woodlands, and scrub", correct: false},
+        ]
+    },
+    {
+        question: "India’s largest petrochemical complex is located at",
+        answers: [
+            { text: "Gujarat", correct: true},
+            { text: "Maharashtra", correct: false},
+            { text: "Rajasthan", correct: false},
+            { text: "Assam", correct: false},
+        ]
+    },
+    {
+        question: "How many Delimitation Commissions have been constituted by the Government of India till December 2023?",
+        answers: [
+            { text: "One", correct: false},
+            { text: "Two", correct: false},
+            { text: "Three", correct: false},
+            { text: "Four", correct: true},
+        ]
+    },
+    {
+        question: "Under which of the following Articls of the Constitution of India, has the Supreme Court of India placed the Right to Privacy?",
+        answers: [
+            { text: "Article 15", correct: false},
+            { text: "Article 16", correct: false},
+            { text: "Article 19", correct: false},
+            { text: "Article 21", correct: true},
+        ]
+    },
+    {
+        question: "Operations undertaken by the Army towards upliftment of the local population in remote areas to include addressing of their basic needs is called:",
+        answers: [
+            { text: "Operation Sankalp", correct: false},
+            { text: "Operation Maitri", correct: false},
+            { text: "Operation Sadbhavana", correct: true},
+            { text: "Operation Madam", correct: false},
+        ]
+    }
+];
+
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextBtn = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz() {
+    currentQuestionIndex = 0;
+    score = 0;
+    nextBtn.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion() {
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+    });
+}
+
+function resetState() {
+    nextBtn.style.display = "none";
+    while(answerButtons.firstChild) {
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+startQuiz();
