7568c25048ac108ad40ab76046c9a4dc72bf802c
Babu Nair <babunairofficial@gmail.com>
2025-04-04 10:48:29 +0530

    Notes App - create inputbox element on click in javascript

---
+++ Notes_App/script.js
@@ -0,0 +1,15 @@
+const notesContainer = document.querySelector(".notes-container");
+const createBtn = document.querySelector(".btn");
+
+let notes = document.querySelectorAll(".input-box"); //input-box class would be added through javascript
+
+//create an input-box class element, inside notes-container class, through javascript
+createBtn.addEventListener("click", () => {
+    let inputBox = document.createElement("p");
+    let img = document.createElement("img");
+    inputBox.className = "input-box";
+    inputBox.setAttribute("contenteditable", "true");
+    img.src = "images/delete.png"
+    notesContainer.appendChild(inputBox).appendChild(img);
+
+})
