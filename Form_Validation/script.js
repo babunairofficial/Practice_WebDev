dd58270dd2b2a56d8dc7d97198cbb0c982e6627c
Babu Nair <babunairofficial@gmail.com>
2025-04-18 18:15:35 +0530

    Form Validation - email error script

--- Form_Validation/script.js
+++ Form_Validation/script.js
@@ -37,3 +37,17 @@ function validatePhone() {
     return true;
 }
 
+function validateEmail() {
+    var email = document.getElementById('contact-email').value;
+
+    if(email.length == 0){
+        emailError.innerHTML = 'Email is required!';
+        return false;
+    }
+    if(!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)){
+        emailError.innerHTML = 'Email Invalid';
+        return false;
+    }
+    emailError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+    return true;
+}
